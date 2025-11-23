06437c0aabb557a73017b391d6b852a7d3523c01
Celo Composer <support@celo.org>
2025-11-23 05:28:24 +0100

    added brianai apikey

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
-  },
+  turbopack: {},
 };
 
-module.exports = nextConfig;
+export default nextConfig;
